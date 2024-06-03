0de7985d85a9db9ca8e20439e1ea4c3a872d8afd
Do-Di <dongc2k34@gmail.com>
2024-06-03 23:48:20 +0700

    update azure storage

--- Infrastructure/Storage/AzureStorage.cs
+++ Infrastructure/Storage/AzureStorage.cs
@@ -1,7 +1,21 @@
-﻿namespace Infrastructure.Storage
+﻿using Microsoft.Extensions.Configuration;
+using Azure.Storage.Blobs;
+namespace Infrastructure.Storage
 {
     public class AzureStorage
     {
-        public AzureStorage() { }
+        private readonly IConfiguration configuration;
+        private readonly string connectionStrsing;
+        private readonly string blobContainer;
+        private readonly string severStorageUri;
+        public AzureStorage(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            connectionStrsing = this.configuration["BLOB_STORAGE_CONNECTION_STRING"] ?? string.Empty;
+            blobContainer = this.configuration["BLOB_CONTAINER"] ?? string.Empty;
+            severStorageUri = this.configuration["SERVER_BLOB_STORAGE_URI"] ?? string.Empty;
+        }
+
+        //public UploadFile(string path, )
     }
 }
